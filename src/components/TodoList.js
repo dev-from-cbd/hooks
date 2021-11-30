7114457243f4aefa987e3f319171fadf9a28f369
dev-from-cbd <veryfatwombat@gmail.com>
2021-12-01 00:18:13 +1100

    react

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,10 +4,21 @@ import TodoForm from "./TodoForm";
 function TodoList() {
   const [todos, setTodos] = useState([]);
 
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    setTodos(newTodos);
+    console.log(...todos);
+  };
+
   return (
     <div>
       <h1>Task Manager</h1>
-      <TodoForm>task #1</TodoForm>
+      <TodoForm onSubmit={addTodo} />
     </div>
   );
 }
